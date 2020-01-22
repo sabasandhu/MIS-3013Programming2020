9f40852f6425e9d844b179f1b0fd61e29286bc92
sand0033 <saba@ou.edu>
2020-01-22 12:54:47 -0600

    created variable for firstname lastname and favoritenumber

--- InClassSolutions/Variables/Program.cs
+++ InClassSolutions/Variables/Program.cs
@@ -11,6 +11,27 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            //Create a string named firstName
+            string firstName;
+
+            //Assign a value to firstName (Saba)
+            firstName = "Saba";
+
+            //Create an int named favoriteNumber and assign your favorite number to it
+            int favoriteNumber = 9 ;
+
+            double crazyNumber = 7.6767 * favoriteNumber;
+
+            string lastName = "Sandhu";
+            string fullName = firstName + " " + lastName;
+
+            Console.WriteLine(fullName + " has the favorite number of " + favoriteNumber
+                        + "\nwhich equates to a crazy number of " + crazyNumber); 
+
+            //MAKE IT DYNAMIC
+            Console.ReadKey();
         }
+
+
     }
 }
