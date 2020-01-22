36eab0bae079676721df66a133f491f600673451
sand0033 <saba@ou.edu>
2020-01-22 12:34:14 -0600

    Created the in class sample solutions and created a solution and project

---
+++ InClassSolutions/Variables/Program.cs
@@ -0,0 +1,16 @@
+﻿//Saba Sandhu
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
